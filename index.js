7099330fdd4bb1f2426dff569b3c1cee51f587fc
EEEEEEEEERqr <108788255+EEEEEEEEERqr@users.noreply.github.com>
2024-04-13 00:28:49 -0600

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Forbidden Shop","Watching Tickets & Claims!"];
 
 
 let currentIndex = 0;
